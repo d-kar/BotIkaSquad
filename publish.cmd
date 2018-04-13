fc52b5d64cb571017dfe2f73d2252434bcc4840e
Oleg Shkvorets <oleg@pcigeomatics.com>
2018-04-13 17:59:47 -0400

    first commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.ProactiveBot.sln -p:DeployOnBuild=true -p:PublishProfile=botikasquad-Web-Deploy.pubxml -p:Password=YjKtWDpENAnbY4kNleqBGCa6pQJLMRY81vHJWceq5vRl7f1ECz1eneCvuXfh
+
